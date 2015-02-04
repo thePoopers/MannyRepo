34b28a62db83d0e6b1fb1c441d769c14f5550b05
Manny Martinez <mannymartinez@mpcen-macbook.local>
2015-02-04 13:01:36 -0500

    Findbook.php & Search.php working

--- webservice/search.php
+++ webservice/search.php
@@ -13,6 +13,33 @@ $contents = $_POST["contents"];
 $output = file_get_contents('http://isbndb.com/api/v2/json/Q0DGGAQJ/books?q='.urlencode($contents).'');
 
 //Spit the output
+//echo "Beginning of OBJECT NOTATION (NOT JSON)";
+echo "\n";
+
+$objJson = json_decode($output);
+
+//Example of printing the title of the 2nd book found
+print_r ($objJson->data[1]->title);
+
+//if statement
+//for statement
+//both of these need to involve page_count, current_page, result_count
+
+echo "\n";
+
+//Print in object notation (non json)
+//print_r ($objJson);
+
+/*
+//Used for Debugging
+echo "\n";
+echo "-----End of Object Notation----------";
+echo "\n";
+echo "Beginning of JSON";
+echo "\n";
+*/
+
+//Print JSON output
 echo $output;
 
 ?>
